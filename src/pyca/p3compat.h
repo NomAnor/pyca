e4d3cc28c7fd6dfadcfe1965f7c8a891bd41b83c
André Althaus <andre.althaus@tu-dortmund.de>
2018-05-08 13:13:28 +0200

    Use consistent string encoding in Python 3
    PyString_FromString converts from UTF-8 char array to python object but
    PyString_AsString converts from python object to ASCII char array.
    Use PyUnicode_AsUTF8 instead.

--- src/pyca/p3compat.h
+++ src/pyca/p3compat.h
@@ -9,28 +9,9 @@
 #define PyInt_AsLong        PyLong_AsLong
 #define PyInt_FromLong      PyLong_FromLong
 #define PyString_FromString PyUnicode_FromString
+#define PyString_AsString   PyUnicode_AsUTF8
 #define PyString_Check      PyUnicode_Check
 #define PyString_FromFormat PyUnicode_FromFormat
-// This should suffice, as long as we don't call this twice and try to hold onto both!
-static char *PyString_AsString(PyObject *o)
-{
-    static char *result = NULL;
-    if (result) {
-        free(result);
-        result = NULL;
-    }
-    if (PyUnicode_Check(o)) {
-        PyObject *temp_bytes = PyUnicode_AsEncodedString(o, "ASCII", "strict");
-        if (temp_bytes != NULL) {
-            result = PyBytes_AS_STRING(temp_bytes);
-            result = strdup(result);
-            Py_DECREF(temp_bytes);
-            return result;
-        }
-        return NULL;
-    }
-    return NULL;
-}
 
 #endif
 
